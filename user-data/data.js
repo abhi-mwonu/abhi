0f6f11c9760e29ffb31c73a60dd8b76e721b17a1
ABHISHEK V <112977586+abhi-mwonu@users.noreply.github.com>
2025-02-21 22:51:59 +0530

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -1,104 +1,34 @@
 export const bio = [
-  "Hello, I'm Vinay Somawat!",
-  "<strong>Frontend Software Engineer</strong> with 4+ years of experience building scalable, user-focused web applications.",
-  "Skilled in Angular, TypeScript, and performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
-  "Passionate about mentoring, teamwork, and continuous improvement in engineering processes.",
+  "Hello, I'm Abhishek v",
+  "<strong>Passionate in Cybersecurity</strong>",
+  "A B.Tech IT student diving into the world of code",
+  "Still a beginner, I get way too excited whenever my code runs on the first try (which, let’s be real, isn’t often)! 😁♥️",
 ];
 
 export const skills = [
-  "JavaScript, TypeScript, Angular, AngularJS, React, HTML5, CSS, Bootstrap",
-  "RxJS, NGXS, NGRX",
-  "Jenkins, JIRA, Webpack, Git",
-];
-
-export const experience = [
-  {
-    title: "Shiprocket Private Limited",
-    duration: "September 2023 - Present",
-    subtitle: "Software Engineer",
-    details: [
-      "Developed a design system using Stencil and Storybook that provides consistent UI components across platforms, ensuring a cohesive design.",
-      "Developed a dynamic support ticket panel that renders subcategories based on component IDs, handling up to 100 AWBs simultaneously.",
-      "Migrated 30+ modules from AngularJS to Angular 15 using Generative AI tools, reporting directly to the VP of Engineering.",
-      "Designed and implemented advanced features such as keyboard shortcuts, improving operational efficiency by 15 percent.",
-      "Took ownership of OKRs, collaborated with the product team, and mentored two junior developers, cutting their code review iterations and ensuring on-time feature delivery.",
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "AngularJs"],
-    icon: "truck ",
-  },
-  {
-    title: "Biofourmis India Private Limited",
-    duration: "April 2022 - Jul 2023",
-    subtitle: "Software Engineer",
-    details: [
-      "Spearheaded the implementation of micro-frontends using Module Federation, enabling modular and scalable architecture.",
-      "Earned the Biofourmis Bravo Award for upgrading the application to support flexible operations across multiple geographies.",
-      "Enhanced performance and user experience by integrating Twilio-Video, Countly, and NGXS WebSocket for real-time data management.",
-    ],
-    tags: [
-      "JavaScript",
-      "Angular",
-      "RxJS",
-      "NGXS",
-      "TypeScript",
-      "RxState",
-      "Webpack",
-      "Optimization",
-    ],
-    icon: "heartbeat",
-  },
-  {
-    title: "Novopay Solutions Private Limited",
-    duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
-    details: [
-      "Built critical banking features including AePS services (eKYC, cash withdrawal, balance inquiry) used by over 1,00,000 retailers.",
-      "Developed QR code integration for wallet money loading, streamlining retailer workflows and reducing errors by 25 percent.",
-      "Integrated Clevertap analytics, Freshdesk ticketing, and chatbot services, enhancing user support and engagement.",
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "qrcode",
-  },
-  {
-    title: "ThinkPedia LLP",
-    duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
-    details: [
-      `Designed and developed a social media post management panel, improving collaboration efficiency by 50 percent.`,
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-    icon: "group",
-  },
-];
-
-export const trekking = [
-  "<strong>Kheerganga Trek</strong>, Himachal Pradesh (9,711 feet)",
-  "<strong>Triund Trek</strong>, Himachal Pradesh (9,350 feet)",
-  "<strong>Kedarkantha Trek</strong>, Uttarakhand (12,500 feet)",
-  "<strong>Jalori Pass Trek</strong>, Himachal Pradesh (10,800 feet)",
-  "<strong>Vaishno Devi Trek</strong>, Jammu & Kashmir (5,200 feet)",
+  "JavaScript, c programming",
+  "Python",
+  "Dbms",
 ];
 
 export const education = [
   {
     title: "B.Tech. in Computer Science and Engineering",
-    duration: "2016 - 2020",
-    subtitle: "National Institute of Technology, Warangal",
+    duration: "2023 - 2027",
+    subtitle: "Government Engineering College Idukki",
     details: [],
     tags: [
       "Data Structures & Algorithms",
-      "Operating Systems",
       "Database Management System",
-      "Computer Networks",
-      "Compiler Designing",
-      "Cloud Computing",
+      "Python",
+      "Computer Organization",
     ],
     icon: "graduation-cap",
   },
   {
-    title: "Class 12th in Science and Mathematics",
+    title: "Class 12th in Biology science with mathematics",
     duration: "",
-    subtitle: "Board of Secondary Education, Rajasthan",
+    subtitle: "Board of Secondary Education, Kerala",
     details: [],
     tags: ["Physics", "Chemistry", "Mathematics"],
     icon: "book",
@@ -111,15 +41,15 @@ export const footer = [
   //     data: [
   //       {
   //         text: "Stackoverflow",
-  //         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+  //         link: "https://stackoverflow.com",
   //       },
   //       {
   //         text: "GitHub",
-  //         link: "https://github.com/vinaysomawat",
+  //         link: "https://github.com/abhi-mwonu",
   //       },
   //       {
   //         text: "LeetCode",
-  //         link: "https://leetcode.com/somawatvinay/",
+  //         link: "https://leetcode.com",
   //       },
   //     ],
   //   },
@@ -136,7 +66,7 @@ export const footer = [
   //       },
   //       {
   //         text: "Clone this page",
-  //         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+  //         link: "https://github.com/abhi-mwonu/abhi.github.io",
   //       },
   //     ],
   //   },
@@ -145,24 +75,24 @@ export const footer = [
     data: [
       //   {
       //     text: "Linkedin",
-      //     link: "https://www.linkedin.com/in/vinaysomawat/",
+      //     link: "https://www.linkedin.com/in/cyberabhi",
       //   },
       //   {
       //     text: "Twitter",
-      //     link: "https://twitter.com/thesigmakid",
+      //     link: "https://twitter.com",
       //   },
       {
         text: "Buy me a coffee",
-        link: "https://www.buymeacoffee.com/r194dME8y",
+        link: "https://www.buymeacoffee.com",
       },
       {
         text: "Github",
-        link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+        link: "https://github.com/abhi-mwonu/abhi.github.io",
       },
     ],
   },
   {
     label: "copyright-text",
-    data: ["Made with &hearts; by Vinay Somawat"],
+    data: ["Made with &hearts; by ABHI"],
   },
 ];
