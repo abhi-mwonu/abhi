7adf32c25de815930494d44833dc07a8d79f27b0
ABHISHEK V <112977586+abhi-mwonu@users.noreply.github.com>
2025-02-22 10:03:02 +0530

    Update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,5 +1,5 @@
 const githubUsername = "abhi-mwonu";
-const mediumUsername = "abhi";
+const mediumUsername = "abhi-mwonu";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
